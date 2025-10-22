f8fc2b36cf6ee69aa0f0ee42af4cf53b9ccd2d3c
GrayBarrow <graybaseball77@gmail.com>
2025-10-22 10:46:32 -0500

    organize files

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from data_loader import load_mnist
-from knn_classifier import train_and_evaluate_knn
-from naive_bayes_classifier import train_and_evaluate_naive_bayes
-from linear_nn_classifier import train_and_evaluate_linear_nn
-from mlp_classifier import train_and_evaluate_MLP
-from cnn_classifier import train_and_evaluate_cnn
+from Classifiers.knn_classifier import train_and_evaluate_knn
+from Classifiers.naive_bayes_classifier import train_and_evaluate_naive_bayes
+from Classifiers.linear_nn_classifier import train_and_evaluate_linear_nn
+from Classifiers.mlp_classifier import train_and_evaluate_MLP
+from Classifiers.cnn_classifier import train_and_evaluate_cnn
 
 def main():
-    X_train, X_test, y_train, y_test = load_mnist('mnist_dataset.npz')
+    X_train, X_test, y_train, y_test = load_mnist('data/mnist_dataset.npz')
     
     print("Training and Evaluating KNN Classifier")
     train_and_evaluate_knn(X_train, X_test, y_train, y_test)
